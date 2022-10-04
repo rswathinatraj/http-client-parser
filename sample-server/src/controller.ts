b955e0e2f43fb2eaa468fbf7750ff13580f3ce92
Swathi Natarajan <rswathinatraj@gmail.com>
2022-10-03 21:09:39 -0500

    Added support for restaurant API

--- sample-server/src/controller.ts
+++ sample-server/src/controller.ts
@@ -1,6 +1,7 @@
 import {Request, Response, Application} from 'express';
-import { area } from './types';
+import { area, restaurant } from './types';
 import AreaData from './area.json';
+import RestaurantData from './restaurant.json';
 
 const controller = (app: Application) => {
     app.get("/", (req:Request, res:Response):void => {
@@ -10,6 +11,17 @@ const controller = (app: Application) => {
     app.get("/areas", (req:Request, res:Response):void => {
         res.header("Access-Control-Allow-Origin", "*")
         res.send({'areas': AreaData.areas as Array<area>});
+    });
+
+    app.get("/areas/:id/restaurants", (req: Request, res:Response):void => {
+        let restaurants = RestaurantData.restaurants as Array<restaurant>;
+        let filteredByArea: restaurant[] = [];
+        restaurants.forEach(restaurant => {
+            if (restaurant.areaId === req.params['id']) {
+                filteredByArea.push(restaurant);
+            }
+        });
+        res.send({'restaurants': filteredByArea});
     });
 };
 
--- sample-server/src/types.ts
+++ sample-server/src/types.ts
@@ -2,3 +2,9 @@ export type area = {
     name: string,
     zipcode: string
 };
+
+export type restaurant = {
+    id: string,
+    name: string,
+    areaId: string
+};
